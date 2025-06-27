b55e1205a042767e87a9000ae4a689690177b227
aseacrest <aseacrest@gmail.com>
2025-06-27 15:15:51 -0400

    Update OrderDataProcessor.cs

--- WF_OrderDataProcessor.Processor/Orders/OrderDataProcessor.cs
+++ WF_OrderDataProcessor.Processor/Orders/OrderDataProcessor.cs
@@ -32,7 +32,7 @@ namespace OrderDataProcessor.Orders
             return [];// Default. Remove to add logic
         }
 
-        public Dictionary<string, (int TotalOrders, decimal TotalAmount)> GetTotalOrdersAndTotalsAmountsPerMonth()
+        public Dictionary<string, (int TotalOrders, decimal TotalAmount)> GetTotalOrdersAndTotalAmountsPerMonth()
         {
             //TODO: Caclulate the total order count and total amount by month
             return [];  // Default. Remove to add logic
